1bdcbdfc160574e26df998eced9b5d6f5085f3fa
Taknax <cankatgames@googlemail.com>
2025-09-25 21:27:54 +0200

    hud update sabers

--- src/main/java/tax/taknax/tax_future_citizen/client/screens/SpaceHelmetOverlayOverlay.java
+++ src/main/java/tax/taknax/tax_future_citizen/client/screens/SpaceHelmetOverlayOverlay.java
@@ -1,9 +1,26 @@
 package tax.taknax.tax_future_citizen.client.screens;
 
+import tax.taknax.tax_future_citizen.procedures.SpaceHelmetOverlayYelliowSaberProcedure;
+import tax.taknax.tax_future_citizen.procedures.SpaceHelmetOverlayWhiteSaberProcedure;
+import tax.taknax.tax_future_citizen.procedures.SpaceHelmetOverlayRedSaberProcedure;
+import tax.taknax.tax_future_citizen.procedures.SpaceHelmetOverlayPurpleSaberProcedure;
+import tax.taknax.tax_future_citizen.procedures.SpaceHelmetOverlayPinkSaberProcedure;
+import tax.taknax.tax_future_citizen.procedures.SpaceHelmetOverlayOrangeSaberProcedure;
+import tax.taknax.tax_future_citizen.procedures.SpaceHelmetOverlayOffSaberProcedure;
+import tax.taknax.tax_future_citizen.procedures.SpaceHelmetOverlayMagentaSaberProcedure;
+import tax.taknax.tax_future_citizen.procedures.SpaceHelmetOverlayLightGreenSaberProcedure;
+import tax.taknax.tax_future_citizen.procedures.SpaceHelmetOverlayLightGraySaberProcedure;
+import tax.taknax.tax_future_citizen.procedures.SpaceHelmetOverlayLightBlueSaberProcedure;
 import tax.taknax.tax_future_citizen.procedures.SpaceHelmetOverlayLeggingsLabelProcedure;
 import tax.taknax.tax_future_citizen.procedures.SpaceHelmetOverlayJetpackLabelProcedure;
 import tax.taknax.tax_future_citizen.procedures.SpaceHelmetOverlayHelmetLabelProcedure;
+import tax.taknax.tax_future_citizen.procedures.SpaceHelmetOverlayGreenSaberProcedure;
+import tax.taknax.tax_future_citizen.procedures.SpaceHelmetOverlayGraySaberProcedure;
+import tax.taknax.tax_future_citizen.procedures.SpaceHelmetOverlayCyanSaberProcedure;
+import tax.taknax.tax_future_citizen.procedures.SpaceHelmetOverlayBrownSaberProcedure;
 import tax.taknax.tax_future_citizen.procedures.SpaceHelmetOverlayBootsLabelProcedure;
+import tax.taknax.tax_future_citizen.procedures.SpaceHelmetOverlayBlueSaberProcedure;
+import tax.taknax.tax_future_citizen.procedures.SpaceHelmetOverlayBlackSaberProcedure;
 
 import org.checkerframework.checker.units.qual.h;
 
@@ -58,6 +75,57 @@ public class SpaceHelmetOverlayOverlay {
 			}
 			if (SpaceHelmetOverlayLeggingsLabelProcedure.execute(entity)) {
 				event.getGuiGraphics().blit(ResourceLocation.parse("tax_future_citizen:textures/screens/space_leggings_item.png"), w / 2 + -207, h / 2 + -76, 0, 0, 16, 16, 16, 16);
+			}
+			if (SpaceHelmetOverlayBlackSaberProcedure.execute(entity)) {
+				event.getGuiGraphics().blit(ResourceLocation.parse("tax_future_citizen:textures/screens/lightsaber_black_item.png"), w / 2 + -207, h / 2 + -40, 0, 0, 16, 16, 16, 16);
+			}
+			if (SpaceHelmetOverlayBlueSaberProcedure.execute(entity)) {
+				event.getGuiGraphics().blit(ResourceLocation.parse("tax_future_citizen:textures/screens/lightsaber_blue_item.png"), w / 2 + -207, h / 2 + -40, 0, 0, 16, 16, 16, 16);
+			}
+			if (SpaceHelmetOverlayLightBlueSaberProcedure.execute(entity)) {
+				event.getGuiGraphics().blit(ResourceLocation.parse("tax_future_citizen:textures/screens/lightsaber_blue_light_item.png"), w / 2 + -207, h / 2 + -40, 0, 0, 16, 16, 16, 16);
+			}
+			if (SpaceHelmetOverlayBrownSaberProcedure.execute(entity)) {
+				event.getGuiGraphics().blit(ResourceLocation.parse("tax_future_citizen:textures/screens/lightsaber_brown_item.png"), w / 2 + -207, h / 2 + -40, 0, 0, 16, 16, 16, 16);
+			}
+			if (SpaceHelmetOverlayCyanSaberProcedure.execute(entity)) {
+				event.getGuiGraphics().blit(ResourceLocation.parse("tax_future_citizen:textures/screens/lightsaber_cyan_item.png"), w / 2 + -207, h / 2 + -40, 0, 0, 16, 16, 16, 16);
+			}
+			if (SpaceHelmetOverlayGraySaberProcedure.execute(entity)) {
+				event.getGuiGraphics().blit(ResourceLocation.parse("tax_future_citizen:textures/screens/lightsaber_gray_item.png"), w / 2 + -207, h / 2 + -40, 0, 0, 16, 16, 16, 16);
+			}
+			if (SpaceHelmetOverlayLightGraySaberProcedure.execute(entity)) {
+				event.getGuiGraphics().blit(ResourceLocation.parse("tax_future_citizen:textures/screens/lightsaber_gray_light_item.png"), w / 2 + -207, h / 2 + -40, 0, 0, 16, 16, 16, 16);
+			}
+			if (SpaceHelmetOverlayGreenSaberProcedure.execute(entity)) {
+				event.getGuiGraphics().blit(ResourceLocation.parse("tax_future_citizen:textures/screens/lightsaber_green_item.png"), w / 2 + -207, h / 2 + -40, 0, 0, 16, 16, 16, 16);
+			}
+			if (SpaceHelmetOverlayLightGreenSaberProcedure.execute(entity)) {
+				event.getGuiGraphics().blit(ResourceLocation.parse("tax_future_citizen:textures/screens/lightsaber_green_light_item.png"), w / 2 + -207, h / 2 + -40, 0, 0, 16, 16, 16, 16);
+			}
+			if (SpaceHelmetOverlayMagentaSaberProcedure.execute(entity)) {
+				event.getGuiGraphics().blit(ResourceLocation.parse("tax_future_citizen:textures/screens/lightsaber_magenta_item.png"), w / 2 + -207, h / 2 + -40, 0, 0, 16, 16, 16, 16);
+			}
+			if (SpaceHelmetOverlayOrangeSaberProcedure.execute(entity)) {
+				event.getGuiGraphics().blit(ResourceLocation.parse("tax_future_citizen:textures/screens/lightsaber_orange_item.png"), w / 2 + -207, h / 2 + -40, 0, 0, 16, 16, 16, 16);
+			}
+			if (SpaceHelmetOverlayPinkSaberProcedure.execute(entity)) {
+				event.getGuiGraphics().blit(ResourceLocation.parse("tax_future_citizen:textures/screens/lightsaber_pink_item.png"), w / 2 + -207, h / 2 + -40, 0, 0, 16, 16, 16, 16);
+			}
+			if (SpaceHelmetOverlayPurpleSaberProcedure.execute(entity)) {
+				event.getGuiGraphics().blit(ResourceLocation.parse("tax_future_citizen:textures/screens/lightsaber_purple_item.png"), w / 2 + -207, h / 2 + -40, 0, 0, 16, 16, 16, 16);
+			}
+			if (SpaceHelmetOverlayRedSaberProcedure.execute(entity)) {
+				event.getGuiGraphics().blit(ResourceLocation.parse("tax_future_citizen:textures/screens/lightsaber_red_item.png"), w / 2 + -207, h / 2 + -40, 0, 0, 16, 16, 16, 16);
+			}
+			if (SpaceHelmetOverlayWhiteSaberProcedure.execute(entity)) {
+				event.getGuiGraphics().blit(ResourceLocation.parse("tax_future_citizen:textures/screens/lightsaber_white_item.png"), w / 2 + -207, h / 2 + -40, 0, 0, 16, 16, 16, 16);
+			}
+			if (SpaceHelmetOverlayYelliowSaberProcedure.execute(entity)) {
+				event.getGuiGraphics().blit(ResourceLocation.parse("tax_future_citizen:textures/screens/lightsaber_yellow_item.png"), w / 2 + -207, h / 2 + -40, 0, 0, 16, 16, 16, 16);
+			}
+			if (SpaceHelmetOverlayOffSaberProcedure.execute(entity)) {
+				event.getGuiGraphics().blit(ResourceLocation.parse("tax_future_citizen:textures/screens/lightsaber_off_item.png"), w / 2 + -212, h / 2 + -35, 0, 0, 16, 16, 16, 16);
 			}
 			if (SpaceHelmetOverlayJetpackLabelProcedure.execute(entity))
 				event.getGuiGraphics().drawString(Minecraft.getInstance().font, Component.translatable("gui.tax_future_citizen.space_helmet_overlay.label_fuel"), w / 2 + -183, h / 2 + -91, -16711936, false);
---
+++ src/main/java/tax/taknax/tax_future_citizen/procedures/SpaceHelmetOverlayOrangeSaberProcedure.java
@@ -0,0 +1,15 @@
+package tax.taknax.tax_future_citizen.procedures;
+
+import tax.taknax.tax_future_citizen.init.TaxFutureCitizenModItems;
+
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.Entity;
+
+public class SpaceHelmetOverlayOrangeSaberProcedure {
+	public static boolean execute(Entity entity) {
+		if (entity == null)
+			return false;
+		return (entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).getItem() == TaxFutureCitizenModItems.LIGHTSABER_ORANGE.get();
+	}
+}
